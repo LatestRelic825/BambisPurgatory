977a1067055a3c0873de667bef08f9327844958e
LatestRelic825 <alesandro.villeda114@gmail.com>
2023-01-31 14:48:37 -0600

    Extinction of Cheemy.

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -18,9 +18,9 @@ using StringTools;
 class CoolUtil
 {
 	public static var defaultDifficulties:Array<String> = [
-		'Easy',
 		'Normal',
-		'Hard'
+		'Hard',
+		'Insane'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
@@ -44,7 +44,7 @@ class CoolUtil
 		}
 		else
 		{
-			fileSuffix = '-hard';
+			fileSuffix = '';
 		}
 		return Paths.formatToSongPath(fileSuffix);
 	}
