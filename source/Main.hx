c31b8ec519904a70071631892f9827e0bf0369e8
LatestRelic825 <alesandro.villeda114@gmail.com>
2023-03-07 08:48:50 -0600

    Long Song Support Code found by Mira.
    Credit goes to the Codename Engine creators for the original code. Engine is linked here.
    
    https://github.com/YoshiCrafter29/CodenameEngine

--- source/Main.hx
+++ source/Main.hx
@@ -104,6 +104,8 @@ class Main extends Sprite
 		#end
 	}
 
+	public static var changeID:Int = 0;
+
 	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
 	// very cool person for real they don't get enough credit for their work
 	#if CRASH_HANDLER
